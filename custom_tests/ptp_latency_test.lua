ebebe2a08fa36e4efc0822bf688158d9b30e1137
felipe.rechia <felipe.rechia@datacom.ind.br>
2018-03-07 20:24:49 -0300

    Create basic ptp based latency test
    This test accepts variable frame size and interface load
    
    Signed-off-by: felipe.rechia <felipe.rechia@datacom.ind.br>

--- custom_tests/ptp_latency_test.lua
+++ custom_tests/ptp_latency_test.lua
@@ -4,11 +4,10 @@ local device = require "device"
 local ts     = require "timestamping"
 local stats  = require "stats"
 local hist   = require "histogram"
+local namespaces = require "namespaces"
 
 local PKT_SIZE	= 3000
 local ETH_DST	= "11:12:13:14:15:16"
-
-tx_running = true
 
 local function getRstFile(...)
 	local args = { ... }
@@ -26,6 +25,7 @@ function configure(parser)
 	parser:argument("rx", "Device to receive from."):convert(tonumber)
 	parser:argument("tx", "Device to transmit to."):convert(tonumber)
 	parser:option("-m --maxrate", "Max transmission rate of the DUT in Mbit/s."):default(40000):convert(tonumber)
+	parser:option("-l --load", "Max transmission rate of the DUT in Mbit/s."):default(10):convert(tonumber)
 	parser:option("-s --framesize", "Frame size"):default(PKT_SIZE):convert(tonumber)
 	parser:option("-f --file", "Filename of the latency histogram."):default("histogram.csv")
 end
@@ -36,37 +36,39 @@ function master(args)
 		os.exit(1)
 	end
 
-	local rxdev = device.config({port = args.rx, rxQueues = 1, txQueues = 1})
+	local rxdev = device.config({port = args.rx, rxQueues = 2, txQueues = 2})
 	local txdev = device.config({port = args.tx, rxQueues = 2, txQueues = 2})
 	device.waitForLinks()
 
 	-- TODO grab all of this stuff from user supplied arguments
 	local maxrate = args.maxrate
-	local load_list = {10}
-	local frame_size_list = {512}
-	local iteration_duration = 5
-	local num_iteration = 1
+	local load = args.load
+	local framesize = args.framesize
+	local test_duration = 5
 
-	for fsindex,framesize in ipairs(frame_size_list) do
-		for lindex,load in ipairs(load_list) do
-			local txrate = maxrate * load / 100
-			print("Frame Size: " .. framesize .. ", Load: " .. load .. ", Tx Rate: " .. txrate)
-			for i=1,num_iteration do
-				print("  Iteration " .. i)
-				txdev:setRate(txrate)
-				-- do we need to have an Rx function?
-				-- mg.startTask("loadSlaveRx", rxdev, args.framesize)
-				mg.startTask("loadSlaveTx", txdev, 0, framesize, iteration_duration)
-				stats.startStatsTask{txdev}
-				mg.startSharedTask("timerSlave", txdev:getTxQueue(1), rxdev:getRxQueue(0), tostring(framesize)..tostring(load)..tostring(i)..args.file)
-				mg.waitForTasks()
-			end
-		end
-	end
+	local sharedspace = namespaces:get("sharedspace")
+
+	local txrate = maxrate * load / 100
+	print("Frame Size: " .. framesize .. ", Load: " .. load .. ", Tx Rate: " .. txrate)
+
+	txdev:setRate(txrate)
+	-- do we need to have an Rx function?
+	-- mg.startTask("loadSlaveRx", rxdev, args.framesize)
+	mg.startTask("loadSlaveTx", txdev, 0, framesize, test_duration)
+	txdev.duration = test_duration
+
+	print("Duration ".. tostring(txdev.duration))
+	print("txdev ".. tostring(txdev))
+	stats.startStatsTask{txdev, duration = test_duration}
+	mg.startSharedTask("timerSlave", txdev:getTxQueue(1), rxdev:getRxQueue(1),
+		tostring(framesize).."_"..tostring(load).."_"..tostring(i).."_"..args.file)
+	mg.waitForTasks()
+
 end
 
 function loadSlaveTx(device, queue_id, frame_size, duration)
-	tx_running = true
+	local sharedspace = namespaces:get("sharedspace")
+	sharedspace.tx_running = true
 	local starttime = os.time()
 	local endtime = os.time() + duration
 	local mem = memory.createMemPool(function(buf)
@@ -81,16 +83,19 @@ function loadSlaveTx(device, queue_id, frame_size, duration)
 		bufs:alloc(frame_size)
 		device:getTxQueue(queue_id):send(bufs)
 	end
-	tx_running = false
+	device:getTxQueue(queue_id):stop()
+	sharedspace.tx_running = false
 end
 
 function timerSlave(txQueue, rxQueue, histfile)
+	local sharedspace = namespaces:get("sharedspace")
 	local timestamper = ts:newTimestamper(txQueue, rxQueue)
 	local hist = hist:new()
 	mg.sleepMillis(1000) -- ensure that the load task is running
-	while mg.running() and tx_running do
+	while sharedspace.tx_running do
 		hist:update(timestamper:measureLatency(function(buf) buf:getEthernetPacket().eth.dst:setString(ETH_DST) end))
 	end
+	txQueue:stop()
 	hist:print()
 	hist:save(histfile)
 end
